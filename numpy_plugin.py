2f6711fb13200371beea1b936f49e17cd0b1f80c
Robert McGibbon <rmcgibbo@gmail.com>
2017-08-31 00:12:11 -0400

    Add all of fromnumeric

--- numpy_plugin.py
+++ numpy_plugin.py
@@ -25,6 +25,8 @@ INT_TO_DIMTYPE = {
         3: 'ThreeD'
 }
 DIMTYPE_TO_INT = {v: k for k, v in INT_TO_DIMTYPE.items()}
+ShapeType = None
+AxesType = None
 
 
 def map_name_to_args(callee, ctx, calltype='function'):
@@ -59,14 +61,79 @@ class HasInstanceQuery(TypeQuery[bool]):
             return super().visit_instance(t)
 
 
-def _InferNdims(funcname: str, return_type_args, bound_args: Dict[str, BoundArgument], ctx: FunctionContext):
+def _InferNdimsFromShape(funcname: str, return_type_args, bound_args: Dict[str, BoundArgument], ctx: FunctionContext):
     dtype, ndim = return_type_args
-    assert ndim.type.name() == '_InferNdims'
-    st = ctx.api.modules['numpy'].names['ShapeType'].type
+    assert ndim.type.name() == '_InferNdimsFromShape' 
+    matches_ShapeType = [f for f in bound_args.values() if f is not None and is_same_type(f.formal_typ, ShapeType)]
+    assert len(matches_ShapeType) == 1
 
-    matches = [f for f in bound_args.values() if f is not None and is_same_type(f.formal_typ, st)]
-    if len(matches) == 1:
-        ndim = infer_ndim(matches[0])
+    m = matches_ShapeType[0]
+    ndim = infer_ndim(m)
+    return dtype, ndim
+
+
+def _InferNdimsReduction(funcname: str, return_type_args, bound_args: Dict[str, BoundArgument], ctx: FunctionContext):
+    dtype, ndim = return_type_args
+    assert ndim.type.name() == '_InferNdimsReduction'
+
+    matches_AxesType = [f for f in bound_args.values() if f is not None and is_same_type(f.formal_typ, AxesType)]
+
+    if len(matches_AxesType) == 0:
+        return dtype, 0
+
+    assert len(matches_AxesType) == 1
+
+    arg = ndim.args[0]
+    if isinstance(arg, Instance):
+        operand_ndim = DIMTYPE_TO_INT[arg.type.name()]
+    else:
+        return dtype, AnyType()
+    
+    if 'keepdims' in bound_args and bound_args['keepdims'] is not None:
+        kdarg = bound_args['keepdims'].arg
+        if isinstance(kdarg, NameExpr):
+            if kdarg.fullname == 'builtins.True':
+                keepdims = True
+            elif kdarg.fullname == 'builtins.False': 
+                keepdims = False
+            else:
+                keepdims = '?'
+        else:
+            keepdims = '?'
+    else:
+        keepdims = False
+
+    if keepdims is True:
+        ndim = operand_ndim
+    elif keepdims is False:
+        m = matches_AxesType[0]
+        if isinstance(m.arg_typ, Instance) and m.arg_typ.type.name() == 'int':
+            # called with axis: int
+            ndim = operand_ndim - 1
+        elif isinstance(m.arg_typ, TupleType):
+            ndim = operand_ndim - m.arg_typ.length()
+        else:
+            raise RuntimeError()
+    else:
+        ndim = 'Any'
+
+    return dtype, ndim
+
+
+def _InferNdimsIfAxisSpecified(funcname: str, return_type_args, bound_args: Dict[str, BoundArgument], ctx: FunctionContext):
+    # if axis is None, the default, we return the first type argument
+    # if axis is an int we return the second type argument
+    dtype, ndim = return_type_args
+    assert ndim.type.name() == '_InferNdimsIfAxisSpecified'
+    assert 'axis' in bound_args
+    if bound_args['axis'] is None or isinstance(bound_args['axis'].arg_typ, NoneTyp):
+        # unspecified or specified as None
+        ndim = ndim.args[0]
+    elif isinstance(bound_args['axis'].arg_typ, Instance) and bound_args['axis'].arg_typ.type.name() == 'int':
+        # passed axis: int
+        ndim = ndim.args[1]
+    else:
+        logging.error('cannot infer')
 
     return dtype, ndim
 
@@ -84,6 +151,62 @@ def _RaiseDim(funcname: str, return_type_args: Tuple, bound_args: Dict[str, Boun
 
     return dtype, ndim
 
+def _LowerDim(funcname: str, return_type_args: Tuple, bound_args: Dict[str, BoundArgument], ctx: FunctionContext):
+    dtype, ndim = return_type_args
+    assert ndim.type.name() == '_LowerDim'
+
+    arg = ndim.args[0]
+    if isinstance(arg, Instance):
+        ndim = DIMTYPE_TO_INT[arg.type.name()] - 1
+    else:
+        ndim = AnyType()
+
+    return dtype, ndim
+
+
+def _LowerDim2(funcname: str, return_type_args: Tuple, bound_args: Dict[str, BoundArgument], ctx: FunctionContext):
+    dtype, ndim = return_type_args
+    assert ndim.type.name() == '_LowerDim2'
+
+    arg = ndim.args[0]
+    if isinstance(arg, Instance):
+        ndim = DIMTYPE_TO_INT[arg.type.name()] - 2
+    else:
+        ndim = AnyType()
+
+    return dtype, ndim
+
+
+def _LowerDimIfAxisSpecified(funcname: str, return_type_args: Tuple, bound_args: Dict[str, BoundArgument], ctx: FunctionContext):
+    type, ndim = return_type_args
+    assert ndim.type.name() == '_LowerDimIfAxisSpecified'
+
+    arg = ndim.args[0]
+    if isinstance(arg, Instance) :
+        ndim = DIMTYPE_TO_INT[arg.type.name()] - 1
+    else:
+        ndim = AnyType()
+
+    return dtype, ndim
+
+
+def _ToggleDims_12_21(funcname: str, return_type_args, bound_args: Dict[str, BoundArgument], ctx: FunctionContext):
+    dtype, ndim = return_type_args
+    assert ndim.type.name() == '_ToggleDims_12_21'
+
+    arg = ndim.args[0]
+    if isinstance(arg, Instance):
+        input_ndim = DIMTYPE_TO_INT[arg.type.name()]
+        if input_ndim == 2:
+            ndim = 1
+        elif input_ndim == 1:
+            ndim = 2
+        else:
+            raise RuntimeError()
+    else:
+        ndim = AnyType()
+
+    return dtype, ndim
 
 def _InferDtype(funcname: str, return_type_args: Tuple, bound_args: Dict[str, BoundArgument], ctx: FunctionContext):
     dtype, ndim = return_type_args
@@ -150,6 +273,8 @@ def _IndexHook(funcname: str, return_type_args: Tuple, bound_args: Dict[str, Bou
             elif isinstance(arg, Instance) and arg.type.name() == 'slice':
                 pass
             else:
+                # Tuple[numpy.ndarray[builtins.int*, numpy.OneD], numpy.ndarray[builtins.int*, numpy.OneD]]
+                # how to deal with this?
                 raise ValueError()
 
 
@@ -157,19 +282,18 @@ def _IndexHook(funcname: str, return_type_args: Tuple, bound_args: Dict[str, Bou
 
 
 
-
-
-
-    import IPython; IPython.embed()
-    raise ValueError('here')
-
-
 class NumpyPlugin(Plugin):
     typefunctions  = {
         'numpy._RaiseDim': _RaiseDim,
-        'numpy._InferNdims': _InferNdims,
+        'numpy._LowerDim': _LowerDim,
+        'numpy._LowerDim2': _LowerDim2,
+        'numpy._InferNdimsFromShape': _InferNdimsFromShape,
+        'numpy._InferNdimsReduction': _InferNdimsReduction,
+        'numpy._InferNdimsIfAxisSpecified': _InferNdimsIfAxisSpecified,
         'numpy._InferDtype': _InferDtype,
         'numpy._InferDtypeWithDefault': _InferDtypeWithDefault,
+        'numpy._ToggleDims_12_21': _ToggleDims_12_21,
+
     }
     special_ndarray_hooks = {
         'numpy.ndarray.__getitem__': _IndexHook,
@@ -185,8 +309,12 @@ class NumpyPlugin(Plugin):
         self.fullname2sig = {}
 
     def do_setup(self, ctx: FunctionContext):
+        global ShapeType, AxesType
         self.api = ctx.api
         self.npmodule = ctx.api.modules['numpy']
+
+        ShapeType = ctx.api.modules['numpy'].names['ShapeType'].type
+        AxesType = ctx.api.modules['numpy'].names['AxesType'].type
 
         hooked_functions = defaultdict(list)
         for node in itertools.chain(self.npmodule.names.values(), self.npmodule.names['ndarray'].node.names.values()):
